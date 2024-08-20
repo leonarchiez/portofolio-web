b89853e9ccd9a0887546383f634a22b99732d955
rzkdriech <ucihasaskee07@gmail.com>
2024-08-20 22:56:16 +0800

    feat: adding custom color and font
    color is base on catppuccin palette and the font si jetbrainsmono

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        base: "#eff1f5",
+        text: "#4c4f69",
+        sky: "#04a5e5"
+      },
+      fontFamily: {
+        jetbrains: "JetBrains Mono"
+      }
+    },
   },
   plugins: [],
 }
