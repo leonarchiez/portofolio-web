255c439c5dd45b1cdf24ee9b422d5a5998f813e6
rzkdriech <ucihasaskee07@gmail.com>
2024-08-20 22:58:52 +0800

    chore: no change
    i thought there are few incorrect breakpoint view port, but it actually
    just a browser issue.. so no change for this

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <nav class="sm:flex sm:justify-between my-7 mx-10 text-text">
+  <nav class="sm:flex sm:justify-between py-7 px-10 text-text">
     <div class="navbar flex justify-between">
       <span class="font-bold">akhdrzki</span>
 
@@ -9,22 +9,22 @@
     </div>
 
     <ul
-      class="sm:flex sm:flex-row sm:gap-12 py-20 gap-14 flex flex-col items-center sm:py-0"
+      class="sm:flex sm:flex-row lg:gap-12 py-20 gap-14 flex flex-col items-center sm:py-0"
     >
       <li
-        class="transition ease-out hover:-translate-y-1 hover:border hover:border-text hover:border-solid hover:py-2 hover:px-3 hover:border-r-4 hover:border-b-4 hover:rounded-md hover:bg-white hover:text-black"
+        class="transition ease-out hover:-translate-y-1 hover:border hover:border-text hover:border-solid hover:py-2 hover:px-3 hover:border-r-4 hover:border-b-4 hover:rounded-md hover:bg-white hover:text-black delay-200"
       >
         <a href="">Project</a>
       </li>
 
       <li
-        class="transition ease-out hover:-translate-y-1 hover:border hover:border-text hover:border-solid hover:py-2 hover:px-3 hover:border-r-4 hover:border-b-4 hover:rounded-md hover:bg-white hover:text-black"
+        class="transition ease-out hover:-translate-y-1 hover:border hover:border-text hover:border-solid hover:py-2 hover:px-3 hover:border-r-4 hover:border-b-4 hover:rounded-md hover:bg-white hover:text-black delay-200"
       >
         <a href="">About</a>
       </li>
 
       <li
-        class="transition ease-out hover:-translate-y-1 hover:border hover:border-text hover:border-solid hover:py-2 hover:px-3 hover:border-r-4 hover:border-b-4 hover:rounded-md hover:bg-white hover:text-black"
+        class="transition ease-out hover:-translate-y-1 hover:border hover:border-text hover:border-solid hover:py-2 hover:px-3 hover:border-r-4 hover:border-b-4 hover:rounded-md hover:bg-white hover:text-black delay-200"
       >
         <a href="">Contact</a>
       </li>
