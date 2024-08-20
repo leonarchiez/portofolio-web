2b44b31c26ef057073268c0433746534b73c54d7
rzkdriech <ucihasaskee07@gmail.com>
2024-08-20 21:49:32 +0800

    initial App
    delete default style scoped, here i use tailwindcss so do not need style
    scoped (vanilla css)

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
-
+  import navbar from "./components/navbar.vue"
 </script>
 
 <template>
+  <header class="font-jetbrains">
+    <navbar></navbar>
+  </header>
 
+  <main class="font-jetbrains">
+  </main>
 </template>
-
-<style scoped>
-</style>
