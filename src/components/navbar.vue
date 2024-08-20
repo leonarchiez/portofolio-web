bd85eac39ef5cb0f8d5df48a0ba3d9b8896d2f92
rzkdriech <ucihasaskee07@gmail.com>
2024-08-20 21:48:02 +0800

    feat: adding navbar
    navbar with simple effect, the menu button to open/close responsive
    navbar will develop soon.

---
+++ src/components/navbar.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts"></script>
+
+<template>
+  <nav class="sm:flex sm:justify-between my-7 mx-10 text-text">
+    <div class="navbar flex justify-between">
+      <span class="font-bold">akhdrzki</span>
+
+      <span class="block sm:hidden"> </span>
+    </div>
+
+    <ul
+      class="sm:flex sm:flex-row sm:gap-12 py-20 gap-14 flex flex-col items-center sm:py-0"
+    >
+      <li
+        class="transition ease-out hover:-translate-y-1 hover:border hover:border-text hover:border-solid hover:py-2 hover:px-3 hover:border-r-4 hover:border-b-4 hover:rounded-md hover:bg-white hover:text-black"
+      >
+        <a href="">Project</a>
+      </li>
+
+      <li
+        class="transition ease-out hover:-translate-y-1 hover:border hover:border-text hover:border-solid hover:py-2 hover:px-3 hover:border-r-4 hover:border-b-4 hover:rounded-md hover:bg-white hover:text-black"
+      >
+        <a href="">About</a>
+      </li>
+
+      <li
+        class="transition ease-out hover:-translate-y-1 hover:border hover:border-text hover:border-solid hover:py-2 hover:px-3 hover:border-r-4 hover:border-b-4 hover:rounded-md hover:bg-white hover:text-black"
+      >
+        <a href="">Contact</a>
+      </li>
+    </ul>
+  </nav>
+</template>
